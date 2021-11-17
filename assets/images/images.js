343b9e28af0ad6b6a974c71a8de30afef7761e91
Ato-D <ddonkoh004@st.ug.edu.gh>
2021-11-17 00:00:32 +0000

    FINAL PUSH DONE

--- assets/images/images.js
+++ assets/images/images.js
@@ -1,9 +1,42 @@
-import homebg from '../bg.jpg'
-import pic from '../profilePic.jpg'
+import pic from '../bg.jpg'
+import bgs from '../bg.jpg'
+import liverpool from '../Liverpool.jpg'
+import chelsea from '../Chelsea.jpg'
+import mancity from '../ManCity.jpg'
+import bayernmunich from '../BayernMunich.jpg'
+import dortmund from '../Dortmund.jpg'
+import leverkusen from '../Leverkusen.jpg'
+import athleticomadrid from '../AtleticoMadrid.png'
+import barcelona from '../Barcelona.jpg'
+import realmadrid from '../RealMadrid.jpg'
+import lyon from '../Lyon.jpg'
+import marselle from '../Marselle.jpg'
+import psg from '../PSG.jpg'
+import acmillan from '../ACMilan.jpg'
+import intermillan from '../InterMilan.jpg'
+import juventus from '../Juventus.jpg'
 
 const images = {
-    backgroundImage: homebg,
-    profilePic: pic
+    backgroundImage: pic,
+    profilePic: pic,
+    authenticationbackground: bgs,
+    liverpool : liverpool,
+    chelsea : chelsea,
+    mancity : mancity,
+    bayernmunich : bayernmunich,
+    dortmund : dortmund,
+    leverkusen : leverkusen,
+    athleticomadrid : athleticomadrid,
+    barcelona : barcelona,
+    realmadrid : realmadrid,
+    lyon : lyon,
+    marselle : marselle,
+    psg : psg,
+    acmillan : acmillan,
+    intermillan : intermillan,
+    juventus : juventus,
+
+
 }
 
 export default images
